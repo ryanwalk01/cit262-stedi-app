454000fa0ef984af03301f95806b393604bb38ca
ryanwalk01 <ryan.walk01@gmail.com>
2023-05-23 11:15:29 -0600

    Added login test

---
+++ stedi-react-native-appstore-main/tests/login.test.js
@@ -0,0 +1,24 @@
+const fetch = require("node-fetch");
+it("Should get a login token",async ()=> {
+    let token = "";
+    const options = {
+        method:"POST",
+        headers:{
+
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify({
+            "userName": "ryan.walk01@gmail.com",
+            "password": "P@ssw0rd"
+          })
+    }
+
+    const response = await fetch("https://dev.stedi.me/login", options);
+
+    token = await response.text();
+    console.log("Token " + token);
+    const status = response.status;
+
+    expect(status).toBe(200);
+    expect(token.length).toBe(36);
+})
